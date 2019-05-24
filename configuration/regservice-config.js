39b359fadeb3ac7135c87107c0da37a8671b6cf4
James <james@quizlicious.com>
2019-05-24 15:31:36 -0400

    Added missing comma

--- configuration/regservice-config.js
+++ configuration/regservice-config.js
@@ -3,27 +3,27 @@ module.exports = {
     cookieSecret: {
         development: "thisIsTheDevCookieSecret",
         production: "**********************************************************************",
-    }
-        mongo: {
-                development: {
-                        connectionString: 'mongodb://regservice:P4ssw0rd!@mongodev:27017/regservice',
-                },
-                production: {
-                        connectionString: 'mongodb://mongousername:mongopassword@mongodns:mongoport/mongoinstance',
-                }
-        },
-        authProviders: {
-            google: {
-               development: {
-                    /* TODO: Make sure nothing sensitive in here */
-                    clientID: '**********************************************************************',
-                    clientSecret: **********************************************************************,
-               },
-               production: {
-                    /* TODO: Make sure nothing sensitive in here */
-                    clientID: '**********************************************************************',
-                    clientSecret: '**********************************************************************',
-               }
+    },
+    mongo: {
+            development: {
+                    connectionString: 'mongodb://regservice:P4ssw0rd!@mongodev:27017/regservice',
+            },
+            production: {
+                    connectionString: 'mongodb://mongousername:mongopassword@mongodns:mongoport/mongoinstance',
             }
+    },
+    authProviders: {
+        google: {
+           development: {
+                /* TODO: Make sure nothing sensitive in here */
+                clientID: '**********************************************************************',
+                clientSecret: **********************************************************************,
+           },
+           production: {
+                /* TODO: Make sure nothing sensitive in here */
+                clientID: '**********************************************************************',
+                clientSecret: '**********************************************************************',
+           }
         }
+    }
 }
